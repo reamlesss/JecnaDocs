d160f05abe90a2aa4e4788180f1ec6fc61260343
Václav Vondráček <reamless@seznam.cz>
2024-11-17 20:04:55 +0100

    app js now runs the whole webapp

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+// Nastavení statických souborů
+app.use(express.static(path.join(__dirname, "../client")));
 
 let clients = new Map();
 
